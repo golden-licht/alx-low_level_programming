f7f05f5747888d7b41878e73d83fd111b259255b
anwarHsultan <luminoonimul@gmail.com>
2023-05-18 19:31:21 +0000

    Print a text, followed by a new line using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the porgram
+ * Return: 0, if the program works without errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
