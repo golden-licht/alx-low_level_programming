40ce4fb19879ecf6d11771d6b7a52b9295c20f9a
anwarHsultan <luminoonimul@gmail.com>
2023-05-21 05:02:59 +0000

    Display the last digit of a random number that is generated at run time

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0, if runs successfully
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int lstdgt = n % 10;
+
+	if (lstdgt > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, lstdgt);
+	}
+	else if (lstdgt == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, lstdgt);
+	}
+	else
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lstdgt);
+	}
+
+	return (0);
+}
