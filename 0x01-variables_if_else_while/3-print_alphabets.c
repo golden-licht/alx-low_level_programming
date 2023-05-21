f6609ccd908927cb16a5f44da3674dcad8382f92
anwarHsultan <luminoonimul@gmail.com>
2023-05-21 05:32:54 +0000

    Print the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0, if successful
+ */
+
+int main(void)
+{
+	char x;
+	int n;
+
+	n = 0;
+	x = 'a';
+
+	while (n < 26)
+	{
+		putchar(x);
+		x++;
+		n++;
+	}
+	n = 0;
+	x = 'A';
+	while (n < 26)
+	{
+		putchar(x);
+		x++;
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
