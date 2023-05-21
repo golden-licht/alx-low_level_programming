a7073ffa43f032e1d6787f591cd3dcc7c24e7356
anwarHsultan <luminoonimul@gmail.com>
2023-05-21 07:30:05 +0000

    Print from 0-9 on a single line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return; 0, if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
