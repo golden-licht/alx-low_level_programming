8b489a247a7b0bb2b42f65ba7d93f5f4c0b21990
anwarHsultan <luminoonimul@gmail.com>
2023-05-24 14:18:42 +0000

    Add description for the parameter of the function _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - lowercase alphabet checker 
+ * _islower - Checker if the passed character is lowercase
+ * @c: any character input
  *
  * Return: 1, if input is lowercase, 0 otherwise
  */
 
-int _islower(int c){
+int _islower(int c)
+{
 	if (c > 96 && c < 123)
 		return (1);
 	return (0);
