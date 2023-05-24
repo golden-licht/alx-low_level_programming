85d4efc88f8dadd60c9bd5d90faf00e1e448d586
anwarHsultan <luminoonimul@gmail.com>
2023-05-24 15:19:41 +0000

    Print the sign of the number passed as argument

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** print_sign - Identifies the sign of a number passed as argument
+/**
+ * print_sign - Identifies the sign of a number passed as argument
  * @n: a number for which sign check is to be done
  *
  * Return: 1, if positive, 0, if 0 and -1, if negative
