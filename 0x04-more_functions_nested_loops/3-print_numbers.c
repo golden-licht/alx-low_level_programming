eea30579b2d04a9f837d8f0a711f2e3f9d97cfad
anwarHsultan <luminoonimul@gmail.com>
2023-05-25 23:31:44 +0000

    Display the numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	x = 48;
+	while (x <= 57)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
