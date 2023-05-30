f4f604f17e00ca5e7731d24d5ffadefb968974b7
anwarHsultan <luminoonimul@gmail.com>
2023-05-30 19:14:11 +0000

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/**
+ * rev_string - Reverse a string
+ * @s: pointer to character set
+ */
+
+void rev_string(char *s)
+{
+	int i, ctr, j;
+	char str[100];
+
+	ctr = 0;
+	/* Determine the length of the string */
+	for (i = 0; s[i] != 0; i++)
+		ctr++;
+	ctr--;
+	/* Copy the string */
+	for (i = 0; s[i] != 0; i++)
+		str[i] = s[i];
+	i = 0;
+	/* Reverse the string */
+	for (j = ctr; j >= 0; j--)
+	{
+		s[i] = str[j];
+		i++;
+	}
+}
