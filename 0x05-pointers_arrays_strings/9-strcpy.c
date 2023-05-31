db428e834478d743c1152ff647f8eb1d70a7d596
anwarHsultan <luminoonimul@gmail.com>
2023-05-31 02:54:44 +0000

    Copy the string pointed to by src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - Copy a string
+ * @src: the pointer to the string to be copied
+ * @dest: to where it is copied to
+ * 
+ * Return: a pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+
+	n = 0;
+	/*Determine lenght of src */
+	for (i = 0; src[i] != 0; i++)
+		n++;
+	/* Copy to dest */
+	for (i = 0; i <= n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
