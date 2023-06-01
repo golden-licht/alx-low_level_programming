a8129a272410aa186076e316ec19d93262e49854
anwarHsultan <luminoonimul@gmail.com>
2023-06-01 06:17:46 +0000

    Reverse the contents of an integer array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array - Reverse the content of array of integers
+ * @a: the array
+ * @n: the number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
