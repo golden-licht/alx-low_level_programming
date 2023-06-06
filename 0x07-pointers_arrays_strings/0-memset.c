595d1d7b143719db78be0dfbcd1056dc27b43154
anwarHsultan <luminoonimul@gmail.com>
2023-06-06 16:16:00 +0000

    Fill the first n bytes of the memory area pointed by s with the constant byte b

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ * _memset - Fill n bytes of memory, pointed by s
+ * @s: pointer to the starting location to be filled
+ * @b: constant char to fill the mermories with
+ * @n: number of bytes of memory to be filled
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return s;
+}
