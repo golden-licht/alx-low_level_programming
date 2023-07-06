9e4d9562a43395f82185c57b6f554a8780c8aeb6
anwarHsultan <luminoonimul@gmail.com>
2023-07-06 01:27:11 +0000

    Compute the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - compute the factorial of a number
+ * @n: the number
+ *
+ * Return: the factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
