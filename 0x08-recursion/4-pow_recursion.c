352a91739e35ca47cbab637d9a1701ba4a7feae8
anwarHsultan <luminoonimul@gmail.com>
2023-07-06 01:37:51 +0000

    Compute a number raised to the power of another number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _pow_recursion - determine @x raised to the power of @y
+ * @x: the number to be raised
+ * @y: the power
+ *
+ * Return: @x the power of @y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
