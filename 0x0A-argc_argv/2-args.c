69facadaee1e57fda4f11de5dbbed19f6e2069e6
anwarHsultan <luminoonimul@gmail.com>
2023-07-14 18:16:37 +0000

    Print the argument passed to it/the program

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h" 
+
+/**
+ * main - print the arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of strings
+ *
+ * Return: 0, if successful
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		while (*argv[i] != '\0')
+		{
+			_putchar(*argv[i]);
+			argv[i]++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
