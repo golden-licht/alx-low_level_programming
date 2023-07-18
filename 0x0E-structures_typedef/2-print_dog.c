ce0177bc5bbd2590c5ca02534deffd80e7846f30
anwarHsultan <luminoonimul@gmail.com>
2023-07-18 18:04:12 +0000

    Print the identifiers for a dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Print the identifier for a dog
+ * @d: a pointer to a dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("nil");
+	if (d->owner == NULL)
+		printf("nil");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
