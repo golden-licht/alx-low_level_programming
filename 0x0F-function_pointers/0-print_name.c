188f934a7bed6b8918fd9ef215f3cd53016fcaad
anwarHsultan <luminoonimul@gmail.com>
2023-07-19 13:17:13 +0000

    Check for NULL argument cases

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - function caller
  * @name: pointer to string/name
@@ -5,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
