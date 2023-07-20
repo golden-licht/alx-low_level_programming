96ba0fa41f7ab5fc97fe1f71f8680159252415ee
anwarHsultan <luminoonimul@gmail.com>
2023-07-20 20:38:05 +0000

    Exclude separator at the end

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,12 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
 		printf("%d", num);
 
-		if (separator != NULL && i < n - 2)
+		if (separator != NULL && i < n -1)
 			printf("%s", separator);
 	}
 	va_end(args);
