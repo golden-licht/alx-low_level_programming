69ccd3852d58db6718a1ee2b687f7c6f1460eca1
anwarHsultan <luminoonimul@gmail.com>
2023-07-20 20:26:08 +0000

    Add Description to the function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * print_numbers - print the integers passed, as argument, to stdout
+ * @separator: what should the separator string between the intgers be?
+ * @n: number of integers
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	int num;
 	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		if (separator == NULL)
-			printf("%d", va_arg(args, int));
-		printf("%d%s",va_arg(args, int), separator);
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(args, int));
 	va_end(args);
+	printf("\n");
 }
