8f3db0b3abf9ea909982450bc8d816dd5ccb13ac
anwarHsultan <luminoonimul@gmail.com>
2023-07-20 02:58:39 +0000

    Function implementations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+/**
+ * op_add - add two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the quotient
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo of two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the remainder
+ */
+
+
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
