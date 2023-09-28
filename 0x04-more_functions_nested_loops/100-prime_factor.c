a62b2a5dac159d38088a0a651bce96f99492fd65
golden-licht <luminoonimul@gmail.com>
2023-09-28 16:04:29 +0300

    Former code doesnt' achieve the intended task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,90 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * is_prime - check if a number is prime
+ * @n: the number for which primality is to be checked
  *
- * Return: 0, if successful
+ * Return: true, if @n is prime, false otherwise
  */
+
+bool is_prime(long int n)
+{
+	int x, i;
+
+	if (n <= 1)
+	{
+		return (false);
+	}
+
+	if (n == 2)
+	{
+		return (true);
+	}
+
+	if (n % 2 == 0)
+	{
+		return (false);
+	}
+
+	x = sqrt(n);
+
+	for (i = 3; i <= x; i += 2)
+	{
+		if (n % i == 0)
+		{
+			return (false);
+		}
+	}
+
+	return (true);
+}
+
+/**
+  * largest_prime_factor - find the largest prime factor of a number
+  * @n: the number
+  *
+  * Return: the largest prime factor of @n
+  */
+
+long int largest_prime_factor(long int n)
+{
+	int x, i;
+	long int largest, j;
+
+	x = sqrt(n);
+	largest = -1;
+
+	for (i = 1; i <= x; i++)
+	{
+		if (n % i == 0)
+		{
+			j = n / i;
+
+			if (is_prime(j) && j > largest)
+			{
+				largest = j;
+			}
+
+			if (is_prime(i) && i > largest)
+			{
+				largest = i;
+			}
+		}
+	}
+
+	return (largest);
+}
+
+/**
+  * main - entry point of the program
+  *
+  * Return: 0 on success
+  */
 
 int main(void)
 {
-	int x, prm, j;
-
-	/* we only need to check upto the square root of the number */
-	for (j = 2; j < 782850; j++)
-	{
-		x = 612852475143 % j;
-		if (x == 0)
-			prm = j;
-	}
-	printf("%d\n", prm);
+	printf("%ld\n", largest_prime_factor(612852475143));
 	return (0);
 }
-
