2e33154cc8e5bb09f3d2c14f74271837338d9dad
golden-licht <luminoonimul@gmail.com>
2023-09-29 17:59:02 +0300

    Find the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+  * sqrt_helper - helper function for _sqrt_recursion
+  * @n: the number for which square root is to be found
+  * @a: lower bound for the sqrt(n)
+  * @b: upper bound for the sqrt(n)
+  *
+  * Description: the function only works for perfect squares,
+  * if @n is not a perfect square it returns -1
+  *
+  * Return: the square root of @n
+  */
+
+int sqrt_helper(int n, int a, int b)
+{
+	int x;
+
+	if (a * a  == n)
+		return (a);
+
+	if (b * b == n)
+		return (b);
+
+	x = (a + b) / 2;
+
+	if (a == x)
+		return (-1);
+
+	if (n >= a * a && n <= x * x)
+		return (sqrt_helper(n, a, x));
+
+	return (sqrt_helper(n, x, b));
+}
+
+/**
+  * _sqrt_recursion - finds the square root of a perfect square
+  * @n: the number for which square root is to be found
+  *
+  * Return: the square root of @n
+  */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_helper(n, 1, n));
+}
