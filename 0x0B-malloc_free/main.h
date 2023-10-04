231bb28c32b68d0579b5fb7dd37f1e6b554ae575
golden-licht <luminoonimul@gmail.com>
2023-10-04 18:54:28 +0300

    Add function prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(int character);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+void char_replace(char *s, int length, char old, char new);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
+#endif 
