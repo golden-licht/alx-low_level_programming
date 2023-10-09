5b43416ed1455d89c7e4a2d9341897c5154fd564
golden-licht <luminoonimul@gmail.com>
2023-10-09 12:59:07 +0300

    Add element of the dog before printing (nil)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("(nil)\n");
+		printf("Name: (nil)\n");
 	if (d->owner == NULL)
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
