72acc0063e662107232fae724a224242dec57a32
golden-licht <luminoonimul@gmail.com>
2023-10-09 12:50:32 +0300

    Print (nil) instead of nil, if one of the elements is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("nil");
+		printf("(nil)");
 	if (d->owner == NULL)
-		printf("nil");
+		printf("(nil)");
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
