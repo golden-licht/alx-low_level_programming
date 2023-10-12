f85863c4219cdf5f894f0a3acebbc6018abcfe63
golden-licht <luminoonimul@gmail.com>
2023-10-12 09:33:10 +0300

    Remove unnecessary condition check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,12 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
-		else
-			printf("%s%d", separator, va_arg(list, int));
+		if (i > 0 && separator)
+			printf("%s", separator);
+
+		printf("%d", va_arg(list, int));
 	}
 
 	va_end(list);
