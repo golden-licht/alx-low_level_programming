5f0625a10177a75898dd778a75c6fba99ceb140a
golden-licht <120052668+golden-licht@users.noreply.github.com>
2023-10-19 18:02:55 +0300

    betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,12 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
 	if (h == NULL)
 		return (0);
-
+	
+	count = 0;
 	do {
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
