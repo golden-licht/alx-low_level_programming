fa481da5bd338f3a02264ed8ad222e736f9cc8d9
golden-licht <luminoonimul@gmail.com>
2023-10-19 19:26:39 +0300

    If str is NULL, then set it's length to 0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,27 @@
 #include "lists.h"
 
+/**
+ * add_node - Add a new node at the beginning of a list_t list
+ * @head: Pointer to pointer of the first node
+ * @str: The string to be stored in the new node
+ *
+ * Return: Pointer to the new node
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *s;
+	size_t len;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	len = (str == NULL) ? 0 : strlen(str);
 	s = strdup(str);
 	new_node->str = s;
-	new_node->len = strlen(s);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
